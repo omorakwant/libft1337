7be8af34bacf6d522f999fecc5ed819ad29987a6
crazyraccon <dahrizcontact@gmail.com>
2024-10-25 17:03:16 +0100

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,11 +6,21 @@
 /*   By: odahriz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 13:12:41 by odahriz           #+#    #+#             */
-/*   Updated: 2024/10/24 15:05:19 by odahriz          ###   ########.fr       */
+/*   Updated: 2024/10/25 17:02:48 by odahriz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	
+	size_t	i;
+	unsigned char	s;
+	unsigned char	d;
+
+	s = src;
+	d = dst;
+	i = 0;
+	while (i < 0)
+	{
+
+	}	
 }
