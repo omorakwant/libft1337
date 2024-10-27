6d2f4714eb942711fd100703a506e245b29f6bf7
omorakwant <omasavalsys@gmail.com>
2024-10-27 18:06:43 +0100

    libft.h modif

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: odahriz <odahriz@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:03:55 by odahriz           #+#    #+#             */
-/*   Updated: 2024/10/27 17:08:52 by odahriz          ###   ########.fr       */
+/*   Updated: 2024/10/27 18:06:24 by odahriz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
-int ft_toupper(int c);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
 
 #endif
