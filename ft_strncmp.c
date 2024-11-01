324e233d1707891938a792874e558470bde23dd9
crazyraccon <dahrizcontact@gmail.com>
2024-11-01 09:39:56 +0100

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,9 +6,27 @@
 /*   By: odahriz <odahriz@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 09:14:45 by odahriz           #+#    #+#             */
-/*   Updated: 2024/11/01 09:14:58 by odahriz          ###   ########.fr       */
+/*   Updated: 2024/11/01 09:39:23 by odahriz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (i < n)
+		{
+			if (s1[i] > s2[i])
+				return (1);
+			else if (s1[i] < s2[i])
+				return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
