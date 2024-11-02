b45c47583f8ab4c216fb199f8764efad70ad9ee9
omorakwant <omasavalsys@gmail.com>
2024-11-02 17:52:36 +0100

    auto_commit

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,13 +6,34 @@
 /*   By: odahriz <odahriz@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 12:34:33 by odahriz           #+#    #+#             */
-/*   Updated: 2024/11/02 14:31:09 by odahriz          ###   ########.fr       */
+/*   Updated: 2024/11/02 17:52:17 by odahriz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memecmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    
+	const unsigned char	*src;
+	const unsigned char	*dst;
+	size_t				i;
+
+	src = (const unsigned char *)s1;
+	dst = (const unsigned char *)s2;
+	i = 0;
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	if (s1 == NULL)
+		return (-1);
+	if (s2 == NULL)
+		return (1);
+	if (n == 0)
+		return (0);
+	while (i < n)
+	{
+		if (src[i] != dst[i])
+			return (src[i] - dst[i]);
+		i++;
+	}
+	return (0);
 }
