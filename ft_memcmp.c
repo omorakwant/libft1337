d5f5821a73de6a4563f1a92343da34eae5306643
omorakwant <omasavalsys@gmail.com>
2024-11-02 12:35:49 +0100

    autocommit

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: odahriz <odahriz@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 09:45:49 by odahriz           #+#    #+#             */
-/*   Updated: 2024/11/02 12:35:28 by odahriz          ###   ########.fr       */
+/*   Created: 2024/11/02 12:34:33 by odahriz           #+#    #+#             */
+/*   Updated: 2024/11/02 12:35:34 by odahriz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	const unsigned char	*src;
-
-	src = (const unsigned char *)s;
-	while (n)
-	{
-		if (*src == (unsigned char)c)
-			return ((void *)src);
-		src++;
-		n--;
-	}
-	return (NULL);
-}
