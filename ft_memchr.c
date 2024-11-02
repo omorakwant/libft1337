ad1c8a80e089c0d42877ff0387c6897650649b66
omorakwant <omasavalsys@gmail.com>
2024-11-02 12:25:18 +0100

    autocommit

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: odahriz <odahriz@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: odahriz <odahriz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 09:45:49 by odahriz           #+#    #+#             */
-/*   Updated: 2024/11/01 18:25:54 by odahriz          ###   ########.fr       */
+/*   Updated: 2024/11/02 12:17:27 by odahriz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	const unsigned char	*src;
 
+	src = (const unsigned char *)s;
+	while (n)
+	{
+		if (*src == (unsigned char)c)
+			return ((void *)src);
+		src++;
+		n--;
+	}
+	return (NULL);
 }
