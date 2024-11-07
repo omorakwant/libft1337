c26bb380f6d101d987475a6fe5ce43b81520848f
omorakwant <omasavalsys@gmail.com>
2024-11-07 13:53:01 +0100

    auto_commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: odahriz <odahriz@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 09:03:55 by odahriz           #+#    #+#             */
-/*   Updated: 2024/10/30 11:14:51 by odahriz          ###   ########.fr       */
+/*   Updated: 2024/11/07 13:48:24 by odahriz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
