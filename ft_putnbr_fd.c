0985cae73a31f5091f428f66918d396d9d2a3740
omorakwant <omasavalsys@gmail.com>
2024-11-07 13:39:58 +0100

    auto_commit

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: odahriz <odahriz@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 13:29:31 by odahriz           #+#    #+#             */
-/*   Updated: 2024/11/07 13:30:03 by odahriz          ###   ########.fr       */
+/*   Updated: 2024/11/07 13:39:55 by odahriz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void ft_putnbr_fd(int n, int fd)
 {
-	
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	char c = (n % 10) + '0';
+	write(fd, &c, 1);
 }
